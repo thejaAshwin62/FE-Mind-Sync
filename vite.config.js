94b854f89964b7039ce0cd40a9297d22ec6867c4
thejaAshwin62 <thejaashwin62@gmail.com>
2025-03-22 14:28:47 +0530

    added server

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api/v1": {
-        target: "http://localhost:5000",
+        target: "https://emb-service.onrender.com",
         changeOrigin: true,
       },
     },

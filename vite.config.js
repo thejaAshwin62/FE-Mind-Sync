87d31efe92eac0a65e16c8196384d02a3bdda495
thejaAshwin62 <thejaashwin62@gmail.com>
2025-03-22 15:21:51 +0530

    env changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "https://emb-service.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
     },
   },
 });

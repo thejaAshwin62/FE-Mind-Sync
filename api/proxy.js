496c6a3d079e08267629f960e006e8a5fc77e604
thejaAshwin62 <thejaashwin62@gmail.com>
2025-03-22 15:31:59 +0530

    env changes3

--- api/proxy.js
+++ api/proxy.js
@@ -1,21 +1,17 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const apiProxy = createProxyMiddleware({
+  target: "https://emb-service.onrender.com",
+  changeOrigin: true,
+  pathRewrite: { "^/api/v1": "/api/v1" },
+  onProxyRes: function (proxyRes) {
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    proxyRes.headers["Access-Control-Allow-Methods"] =
+      "GET, POST, PUT, DELETE, OPTIONS, PATCH";
+    proxyRes.headers["Access-Control-Allow-Headers"] =
+      "X-Requested-With, Content-Type, Accept, Authorization";
+    proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
+  },
+});
 
-module.exports = (req, res) => {
-  let target = 'https://emb-service.onrender.com';
-  
-  // Create proxy
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      '^/api/v1': '/api/v1',
-    },
-    onProxyRes: function(proxyRes, req, res) {
-      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-      proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS, PATCH';
-      proxyRes.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type, Accept, Authorization';
-      proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
-    }
-  })(req, res);
-};
+export default (req, res) => apiProxy(req, res);
